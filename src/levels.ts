631ad8caeceeaf422fe819802b5f928be893b9d6
Patricia Jacob <patricia@celeritas.co.uk>
2026-02-10 18:55:06 +0000

    feat(levels): added a lot more levels

--- src/levels.ts
+++ src/levels.ts
@@ -35,6 +35,308 @@ export const LEVELS: LevelData[] = [
     goals: [{ x: 6, y: 0 }],
   },
   {
+    id: 2,
+    name: 'Basic',
+    gridWidth: 3,
+    gridHeight: 5,
+    playerStart: {
+      x: 0,
+      y: 0,
+    },
+    goals: [
+      {
+        x: 1,
+        y: 3,
+      },
+      {
+        x: 2,
+        y: 4,
+      },
+      {
+        x: 2,
+        y: 3,
+      },
+    ],
+    levelText: 'You may need to deploy multiple S3 buckets',
+    s3Buckets: [
+      {
+        x: 1,
+        y: 2,
+      },
+      {
+        x: 2,
+        y: 1,
+      },
+      {
+        x: 1,
+        y: 1,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Tutorial 2',
+    gridWidth: 6,
+    gridHeight: 3,
+    playerStart: {
+      x: 0,
+      y: 0,
+    },
+    goals: [
+      {
+        x: 5,
+        y: 2,
+      },
+    ],
+    levelText:
+      'And sometimes even fill your bucket with the necessary files before deploying it...',
+    walls: [
+      {
+        x: 1,
+        y: 0,
+      },
+      {
+        x: 1,
+        y: 1,
+      },
+      {
+        x: 3,
+        y: 1,
+      },
+      {
+        x: 4,
+        y: 1,
+      },
+      {
+        x: 5,
+        y: 1,
+      },
+      {
+        x: 5,
+        y: 0,
+      },
+    ],
+    files: [
+      {
+        x: 4,
+        y: 0,
+      },
+    ],
+    s3Buckets: [
+      {
+        x: 3,
+        y: 2,
+        capacity: 1,
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Basic 2',
+    gridWidth: 6,
+    gridHeight: 6,
+    playerStart: {
+      x: 0,
+      y: 0,
+    },
+    goals: [
+      {
+        x: 5,
+        y: 0,
+      },
+      {
+        x: 0,
+        y: 5,
+      },
+      {
+        x: 5,
+        y: 5,
+      },
+    ],
+    levelText: 'If you ever get stuck, you can always restart by pressing R',
+    walls: [
+      {
+        x: 1,
+        y: 0,
+      },
+      {
+        x: 1,
+        y: 1,
+      },
+      {
+        x: 0,
+        y: 3,
+      },
+      {
+        x: 1,
+        y: 3,
+      },
+      {
+        x: 2,
+        y: 1,
+      },
+      {
+        x: 4,
+        y: 0,
+      },
+      {
+        x: 4,
+        y: 4,
+      },
+    ],
+    files: [
+      {
+        x: 2,
+        y: 0,
+      },
+    ],
+    s3Buckets: [
+      {
+        x: 1,
+        y: 2,
+      },
+      {
+        x: 4,
+        y: 2,
+        capacity: 1,
+      },
+      {
+        x: 4,
+        y: 3,
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: 'Tutorial holes',
+    gridWidth: 5,
+    gridHeight: 5,
+    playerStart: {
+      x: 0,
+      y: 0,
+    },
+    goals: [
+      {
+        x: 4,
+        y: 4,
+      },
+    ],
+    levelText: 'Be careful to not fall into holes',
+    s3Buckets: [
+      {
+        x: 4,
+        y: 2,
+      },
+    ],
+    holes: [
+      {
+        x: 1,
+        y: 0,
+      },
+      {
+        x: 1,
+        y: 1,
+      },
+      {
+        x: 1,
+        y: 2,
+      },
+      {
+        x: 1,
+        y: 3,
+      },
+      {
+        x: 3,
+        y: 1,
+      },
+      {
+        x: 3,
+        y: 4,
+      },
+      {
+        x: 3,
+        y: 3,
+      },
+      {
+        x: 3,
+        y: 2,
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: 'Tutorial step functions',
+    gridWidth: 5,
+    gridHeight: 5,
+    playerStart: {
+      x: 0,
+      y: 0,
+    },
+    goals: [
+      {
+        x: 4,
+        y: 0,
+      },
+      {
+        x: 4,
+        y: 4,
+      },
+    ],
+    levelText: 'But worry not because step functions... well... they create steps for you',
+    walls: [
+      {
+        x: 3,
+        y: 3,
+      },
+      {
+        x: 1,
+        y: 4,
+      },
+    ],
+    s3Buckets: [
+      {
+        x: 3,
+        y: 2,
+      },
+      {
+        x: 1,
+        y: 0,
+      },
+    ],
+    holes: [
+      {
+        x: 1,
+        y: 1,
+      },
+      {
+        x: 2,
+        y: 1,
+      },
+      {
+        x: 1,
+        y: 2,
+      },
+      {
+        x: 2,
+        y: 2,
+      },
+      {
+        x: 1,
+        y: 3,
+      },
+      {
+        x: 2,
+        y: 3,
+      },
+    ],
+    stepFunctions: [
+      {
+        x: 0,
+        y: 4,
+      },
+    ],
+  },
+  {
     id: 11,
     name: 'Choose Wisely',
     gridWidth: 12,
